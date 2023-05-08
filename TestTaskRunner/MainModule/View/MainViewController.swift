a49afa89c94e0c36a6dd4ff002f8612cadeb7095
etozhekolyan <etozhekolyan@hotmail.com>
2023-05-08 21:27:27 +0500

    Commit

--- TestTaskRunner/MainModule/View/MainViewController.swift
+++ TestTaskRunner/MainModule/View/MainViewController.swift
@@ -154,13 +154,11 @@ extension UIImageView {
     func imageFromServerURL(_ URLString: String, placeHolder: UIImage?) {
         
         self.image = nil
-        //If imageurl's imagename has space then this line going to work for this
         let imageServerUrl = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         
         if let url = URL(string: imageServerUrl) {
             URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
                 
-                //print("RESPONSE FROM API: \(response)")
                 if error != nil {
                     print("ERROR LOADING IMAGES FROM URL: \(error)")
                     DispatchQueue.main.async {
